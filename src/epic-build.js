df584f133f5d683c86fc7b629b5702dab3cad979
Jeremy Faucher (MINDTREE LIMITED) <v-jfaucher@microsoft.com>
2025-03-18 21:27:21 -0700

    version 0.11.0

--- src/epic-build.js
+++ src/epic-build.js
@@ -10,12 +10,7 @@ const epicConfig = require('../../../epicConfig');
 const epicThemeDir = epicConfig.epicThemeDir;
 const sassFilePath = `${epicThemeDir}/pre-light.css`;
 const projectStyleDir = epicConfig.projectStyleDir;
-
-// Check if the build folder exists, if not, create it
-// if (!fs.existsSync(buildFolderPath)) {
-//   fs.mkdirSync(buildFolderPath, { recursive: true });
-//   console.log(`Build folder created at ${buildFolderPath}`);
-// }
+const targetStylePath = path.join(projectStyleDir, 'style.css');
 
 // Check if the Sass file exists, if not, create an empty one
 if (!fs.existsSync(sassFilePath)) {
@@ -24,7 +19,7 @@ if (!fs.existsSync(sassFilePath)) {
 }
 
 // Compile Sass file
-exec(`sass --style expanded --source-map --embed-sources --no-error-css --quiet ${epicThemeDir}/index.scss:${sassFilePath} && cp ${sassFilePath} ${projectStyleDir}`, (error, stdout, stderr) => {
+exec(`sass --style expanded --source-map --embed-sources --no-error-css --quiet ${epicThemeDir}/index.scss:${sassFilePath}`, (error, stdout, stderr) => {
   if (error) {
     console.error(`Error: ${error.message}`);
     return;
@@ -33,18 +28,23 @@ exec(`sass --style expanded --source-map --embed-sources --no-error-css --quiet 
     console.error(`stderr: ${stderr}`);
     return;
   }
-  // Handle successful execution
   
-  console.log(`Sass file compiled successfully to CSS in projectDir ${sassFilePath}`);
+  console.log(`Sass file compiled successfully to CSS at ${sassFilePath}`);
   
   // Read the compiled Sass file content
   const sassFileContent = fs.readFileSync(sassFilePath, 'utf8');
   
-  // Process the Sass file content (add additional processing here if needed)
-  // For example, extract utility classes, merge with existing styles, etc.
-  
-  // Write the processed content back to the file
+  // Process the Sass file content if needed
   fs.writeFileSync(sassFilePath, sassFileContent, 'utf8');
   
   console.log(`Processed Sass file content written back to ${sassFilePath}`);
+  
+  // Copy the file to the project style directory
+  fs.copyFile(sassFilePath, targetStylePath, (err) => {
+    if (err) {
+      console.error(`File copy failed: ${err.message}`);
+    } else {
+      console.log(`CSS file copied successfully to ${targetStylePath}`);
+    }
+  });
 });
